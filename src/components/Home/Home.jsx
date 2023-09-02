4123106689e453cf8d0e5e74364fb7c992232a19
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2023-09-02 16:06:21 +0500

    added cup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import OrderNow from "./components/ordernow";
+import cupphoto from "../../../public/images/cup.png"
+import v1 from "../../../public/images/v1.svg"
+import v2 from "../../../public/images/v2.svg"
+// import v3 from "../../../public/images/v3.svg"
 
 export default function Home() {
     return (
-        <div className="flex w-full bg-[#F6EBDA] pt-[120px] pb-[120px]">
+        <div className="flex w-full bg-[#F6EBDA] pt-[120px] pb-[120px] space-x-[750px]">
             <div id="text" className="">
-                <div className="flex-col space-y-10 pl-[120px]">
+                <div className="flex-col space-y-20 pl-[120px]">
                     <p className="font-poppins text-5xl w-[480px]">
                         Enjoy your <span className="text-primary">coffee</span> before your activity
                     </p>
@@ -22,6 +26,9 @@ export default function Home() {
                 </div>
             </div>
             <div id="image">
+                <div className="inline-flex h-[416px] w-[416px] bg-[#2F2105] rounded-full justify-center items-center">
+                    <img src={cupphoto} alt="" />
+                </div>
 
             </div>
         </div>
